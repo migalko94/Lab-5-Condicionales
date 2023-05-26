4a1edab022e5fbd9cb76c75d657942ea525def75
migalko94 <miguelgalvezugr@gmail.com>
2023-05-26 10:25:52 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,100 +1,114 @@
 let puntuacion: number = 0;
 
-const asDeCopas = document.getElementById("miniatura1");
-const dosDeCopas = document.getElementById("miniatura2");
-const tresDeCopas = document.getElementById("miniatura3");
-const cuatroDeCopas = document.getElementById("miniatura4");
-const cincoDeCopas = document.getElementById("miniatura5");
-const seisDeCopas = document.getElementById("miniatura6");
-const sieteDeCopas = document.getElementById("miniatura7");
-const sotaDeCopas = document.getElementById("miniatura10");
-const caballoDeCopas = document.getElementById("miniatura11");
-const reyDeCopas = document.getElementById("miniatura12");
+const generarNumeroAleatorio = (): number => {
+  return Math.floor(Math.random() * 10 + 1);
+};
+
+const transformaNumeroAleatorio = (cartaGenerada: number): number => {
+  return cartaGenerada > 7 ? cartaGenerada + 2 : cartaGenerada;
+};
+
+const generarPuntuacion = (cartaGenerada: number): number => {
+  return cartaGenerada < 10
+    ? (puntuacion = puntuacion + cartaGenerada)
+    : (puntuacion = puntuacion + 0.5);
+};
+
+const mostrarMensaje = (mensaje: string): void => {
+  const elementoPuntuacion = document.getElementById("puntuacion");
+  elementoPuntuacion
+    ? (elementoPuntuacion.innerHTML = mensaje)
+    : console.error(
+        "muestraMensajeComprobacion: No se ha encontrado el elemento con id puntuación"
+      );
+};
+
+const mostrarPuntuacion = (): void => {
+  let mensaje: string = `Tu puntuación es ${puntuacion}`;
+  mostrarMensaje(mensaje);
+};
+
+const resultadoPartida = () => {
+  if (puntuacion > 7.5) {
+    gameOver();
+  }
+  if (puntuacion === 7.5) {
+    partidaGanada();
+  }
+};
+
+const partidaGanada = () => {
+  mostrarPuntuacion();
+  let mensaje: string = `${puntuacion} ¡Lo has clavado! ¡Enhorabuena! 🎉🎉🎉`;
+  mostrarMensaje(mensaje);
+
+  gestionarResultado();
+};
+
+const gameOver = () => {
+  mostrarPuntuacion();
+  let mensaje: string = `${puntuacion} Te has pasado 🪦 GAME OVER`;
+  mostrarMensaje(mensaje);
+
+  gestionarResultado();
+};
+
+const cambiarDisplay = (idMiniatura: string): void => {
+  let cartaMostrada = document.getElementById("carta-mostrada");
+  const miniatura = document.getElementById(idMiniatura);
+  if (
+    cartaMostrada &&
+    miniatura &&
+    cartaMostrada instanceof HTMLImageElement &&
+    miniatura instanceof HTMLImageElement
+  ) {
+    cartaMostrada.src = miniatura.src;
+  }
+};
 
 const mostrarCarta = (cartaGenerada: number): void => {
-  let cartaMostrada = document.getElementById("carta-mostrada");
   switch (cartaGenerada) {
-    case (cartaGenerada = 1):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        asDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = asDeCopas.src;
-      }
+    case 1:
+      cambiarDisplay("miniatura1");
+
       break;
-    case (cartaGenerada = 2):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        dosDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = dosDeCopas.src;
-      }
+    case 2:
+      cambiarDisplay("miniatura2");
+
       break;
-    case (cartaGenerada = 3):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        tresDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = tresDeCopas.src;
-      }
+    case 3:
+      cambiarDisplay("miniatura3");
+
       break;
 
-    case (cartaGenerada = 4):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        cuatroDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = cuatroDeCopas.src;
-      }
+    case 4:
+      cambiarDisplay("miniatura4");
+
       break;
-    case (cartaGenerada = 5):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        cincoDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = cincoDeCopas.src;
-      }
+    case 5:
+      cambiarDisplay("miniatura5");
+
       break;
-    case (cartaGenerada = 6):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        seisDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = seisDeCopas.src;
-      }
+    case 6:
+      cambiarDisplay("miniatura6");
+
       break;
 
-    case (cartaGenerada = 7):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        sieteDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = sieteDeCopas.src;
-      }
+    case 7:
+      cambiarDisplay("miniatura7");
+
       break;
-    case (cartaGenerada = 10):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        sotaDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = sotaDeCopas.src;
-      }
+    case 10:
+      cambiarDisplay("miniatura10");
+
       break;
-    case (cartaGenerada = 11):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        caballoDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = caballoDeCopas.src;
-      }
+    case 11:
+      cambiarDisplay("miniatura11");
+
       break;
-    case (cartaGenerada = 12):
-      if (
-        cartaMostrada instanceof HTMLImageElement &&
-        reyDeCopas instanceof HTMLImageElement
-      ) {
-        cartaMostrada.src = reyDeCopas.src;
-      }
+    case 12:
+      cambiarDisplay("miniatura12");
+
       break;
 
     default:
@@ -106,156 +120,76 @@ const mostrarCarta = (cartaGenerada: number): void => {
 };
 
 const dameCarta = (): void => {
-  let cartaGenerada = Math.floor(Math.random() * 10 + 1);
-
-  if (cartaGenerada > 7) {
-    cartaGenerada = cartaGenerada + 2;
-  } else {
-    cartaGenerada = cartaGenerada;
-  }
-
-  console.log(cartaGenerada);
-
+  let cartaGenerada = generarNumeroAleatorio();
+  cartaGenerada = transformaNumeroAleatorio(cartaGenerada);
   mostrarCarta(cartaGenerada);
-  if (cartaGenerada < 10) {
-    puntuacion = puntuacion + cartaGenerada;
-  } else puntuacion = puntuacion + 0.5;
-
-  muestraPuntuacion();
-  gestionarGameOver();
+  generarPuntuacion(cartaGenerada);
+  mostrarPuntuacion();
+  resultadoPartida();
 };
 
-const muestraPuntuacion = (): void => {
-  const elementoPuntuacion = document.getElementById("puntuacion");
+const gestionarResultado = () => {
+  deshabilitaBotonComprobarCarta();
+  deshabilitaBotonMePlanto();
+  nuevaPartida();
+};
+
+const mePlanto = () => {
+  mostrarPuntuacion();
   let mensaje: string = `Tu puntuación es ${puntuacion}`;
-  if (elementoPuntuacion) {
-    elementoPuntuacion.innerHTML = mensaje;
-  } else {
-    console.error(
-      "muestraMensajeComprobacion: No se ha encontrado el elemento con id puntuación"
-    );
+  mostrarMensaje(mensaje);
+
+  if (puntuacion < 4) {
+    mensaje = `${puntuacion} Has sido muy conservador 😐`;
+    mostrarMensaje(mensaje);
+  }
+
+  if (puntuacion >= 4 && puntuacion < 6) {
+    mensaje = `${puntuacion} Te ha entrado el canguelo eh? 😉`;
+    mostrarMensaje(mensaje);
+  }
+  if (puntuacion >= 6 && puntuacion <= 7) {
+    mensaje = `${puntuacion} Casi casi...🫣🫣`;
+    mostrarMensaje(mensaje);
+  }
+
+  resultadoPartida();
+  nuevaPartida();
+
+  deshabilitaBotonComprobarCarta();
+  deshabilitaBotonMePlanto();
+
+  queHabriaPasado();
+};
+
+const habilitaBotonComprobarCarta = () => {
+  if (botonComprobarCarta && botonComprobarCarta instanceof HTMLButtonElement) {
+    botonComprobarCarta.disabled = false;
+  }
+};
+const habilitaBotonMePlanto = () => {
+  if (botonMePlanto && botonMePlanto instanceof HTMLButtonElement) {
+    botonMePlanto.disabled = false;
   }
 };
 
-const gestionarGameOver = () => {
-  const elementoPuntuacion = document.getElementById("puntuacion");
-  let mensaje: string = `Tu puntuación es ${puntuacion}`;
-  if (puntuacion > 7.5) {
-    if (
-      elementoPuntuacion &&
-      botonComprobarCarta &&
-      botonComprobarCarta instanceof HTMLButtonElement &&
-      botonMePlanto instanceof HTMLButtonElement
-    ) {
-      mensaje = `${puntuacion} Te has pasado 🪦 GAME OVER`;
-      elementoPuntuacion.innerHTML = mensaje;
-      botonComprobarCarta.disabled = true;
-      botonMePlanto.disabled = true;
-      nuevaPartida();
-    }
-  } else {
-    console.error(
-      "gestionarGameOver: no se ha encontrado el elemento con id dame-carta"
-    );
+const deshabilitaBotonComprobarCarta = () => {
+  if (botonComprobarCarta && botonComprobarCarta instanceof HTMLButtonElement) {
+    botonComprobarCarta.disabled = true;
   }
 };
 
-document.addEventListener("DOMContentLoaded", muestraPuntuacion);
+const deshabilitaBotonMePlanto = () => {
+  if (botonMePlanto && botonMePlanto instanceof HTMLButtonElement) {
+    botonMePlanto.disabled = true;
+  }
+};
+
+document.addEventListener("DOMContentLoaded", mostrarPuntuacion);
 const botonComprobarCarta = document.getElementById("dame-carta");
 if (botonComprobarCarta && botonComprobarCarta instanceof HTMLButtonElement) {
   botonComprobarCarta.addEventListener("click", dameCarta);
 }
-
-const mePlanto = () => {
-  const elementoPuntuacion = document.getElementById("puntuacion");
-  let mensaje: string = `Tu puntuación es ${puntuacion}`;
-  if (elementoPuntuacion) {
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-
-  if (puntuacion < 4 && elementoPuntuacion) {
-    mensaje = `${puntuacion} Has sido muy conservador 😐`;
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-
-  if (puntuacion >= 4 && puntuacion < 6 && elementoPuntuacion) {
-    mensaje = `${puntuacion} Te ha entrado el canguelo eh? 😉`;
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-  if (puntuacion >= 6 && puntuacion <= 7 && elementoPuntuacion) {
-    mensaje = `${puntuacion} Casi casi...🫣🫣`;
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-
-  if (puntuacion === 7.5 && elementoPuntuacion) {
-    mensaje = `${puntuacion} ¡Lo has clavado! ¡Enhorabuena! 🎉🎉🎉`;
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-
-  if (botonComprobarCarta && botonComprobarCarta instanceof HTMLButtonElement) {
-    botonComprobarCarta.disabled = true;
-  }
-
-  nuevaPartida();
-  queHabriaPasado();
-};
-
-let botonNuevaPartidaCreado = false;
-let botonQueHabriaPasadoCreado = false;
-
-const nuevaPartida = () => {
-  if (!botonNuevaPartidaCreado) {
-    const botonNuevaPartida = document.createElement("button");
-    botonNuevaPartida.setAttribute("id", "boton-nueva-partida");
-    botonNuevaPartida.innerHTML = "Nueva partida 🆕";
-    botonNuevaPartida.setAttribute("onclick", "location.reload()");
-
-    const divBotones = document.getElementById("botones");
-    if (divBotones) {
-      divBotones.appendChild(botonNuevaPartida);
-    }
-    botonNuevaPartidaCreado = true;
-  }
-};
-
-const queHabriaPasado = () => {
-  if (!botonQueHabriaPasadoCreado) {
-    const botonQueHabriaPasado = document.createElement("button");
-    botonQueHabriaPasado.setAttribute("id", "boton-nuevo");
-
-    botonQueHabriaPasado.innerHTML = "🤔 Si hubiese pedido una carta más...";
-    const divBotones = document.getElementById("botones");
-    if (divBotones) {
-      divBotones.appendChild(botonQueHabriaPasado);
-    }
-
-    if (botonMePlanto && botonMePlanto instanceof HTMLButtonElement) {
-      botonQueHabriaPasado.addEventListener("click", mostrarQueHabriaPasado);
-      botonQueHabriaPasado.addEventListener(
-        "click",
-        () => (botonQueHabriaPasado.disabled = true)
-      );
-    }
-    botonQueHabriaPasadoCreado = true;
-  }
-};
-
-const mostrarQueHabriaPasado = () => {
-  dameCarta();
-  muestraPuntuacion();
-  const elementoPuntuacion = document.getElementById("puntuacion");
-  let mensaje: string = `Tu puntuación es ${puntuacion}`;
-
-  if (puntuacion > 7.5 && elementoPuntuacion) {
-    mensaje = `${puntuacion} Te habrías pasado 🪦 GAME OVER`;
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-
-  if (puntuacion === 7.5 && elementoPuntuacion) {
-    mensaje = `${puntuacion} Habrías ganado! 🫠`;
-    elementoPuntuacion.innerHTML = mensaje;
-  }
-};
 
 const botonMePlanto = document.getElementById("me-planto");
 if (botonMePlanto && botonMePlanto instanceof HTMLButtonElement) {
@@ -266,3 +200,111 @@ if (botonMePlanto && botonMePlanto instanceof HTMLButtonElement) {
     }
   });
 }
+
+const puntuacionACero = () => {
+  puntuacion = 0;
+};
+
+const reinicioImagen = () => {
+  const cartaBocaAbajo = "contenido/back.jpg";
+  let cartaMostrada = document.getElementById("carta-mostrada");
+  if (cartaMostrada && cartaMostrada instanceof HTMLImageElement) {
+    cartaMostrada.src = cartaBocaAbajo;
+  }
+};
+
+const comprobarbotonQueHabriaPasado = () => {
+  const botonQueHabriaPasado = document.getElementById("boton-nuevo");
+  if (botonQueHabriaPasado) {
+    botonQueHabriaPasado.remove();
+    botonQueHabriaPasadoCreado = false;
+  }
+};
+
+const eliminaBotonNuevaPartida = () => {
+  const botonNuevaPartida = document.getElementById("boton-nueva-partida");
+  botonNuevaPartida?.remove();
+};
+
+let botonNuevaPartidaCreado = false;
+
+const handlerNuevaPartida = () => {
+  reinicioImagen();
+  puntuacionACero();
+  habilitaBotonComprobarCarta();
+  habilitaBotonMePlanto();
+  mostrarPuntuacion();
+  comprobarbotonQueHabriaPasado();
+  botonNuevaPartidaCreado = false;
+  eliminaBotonNuevaPartida();
+};
+
+const nuevaPartida = () => {
+  if (!botonNuevaPartidaCreado) {
+    const botonNuevaPartida = document.createElement("button");
+    botonNuevaPartida.setAttribute("id", "boton-nueva-partida");
+    botonNuevaPartida.innerHTML = "Nueva partida 🆕";
+    botonNuevaPartida.addEventListener("click", handlerNuevaPartida);
+
+    const divBotones = document.getElementById("botones");
+    if (divBotones) {
+      divBotones.appendChild(botonNuevaPartida);
+    }
+    botonNuevaPartidaCreado = true;
+  }
+};
+
+let botonQueHabriaPasadoCreado = false;
+
+const eliminaBotonQueHabriaPasado = () => {
+  const botonQueHabriaPasado = document.getElementById("boton-nuevo");
+  botonQueHabriaPasado?.remove();
+};
+
+const handlerQueHabriaPasado = () => {
+  mostrarQueHabriaPasado();
+  botonQueHabriaPasadoCreado = false;
+  eliminaBotonQueHabriaPasado();
+};
+
+const queHabriaPasado = () => {
+  if (!botonQueHabriaPasadoCreado) {
+    const botonQueHabriaPasado = document.createElement("button");
+    botonQueHabriaPasado.setAttribute("id", "boton-nuevo");
+    botonQueHabriaPasado.innerHTML = "🤔 Si hubiese pedido una carta más...";
+    const divBotones = document.getElementById("botones");
+    if (divBotones) {
+      divBotones.appendChild(botonQueHabriaPasado);
+    }
+
+    botonQueHabriaPasado.addEventListener("click", handlerQueHabriaPasado);
+
+    botonQueHabriaPasadoCreado = true;
+  }
+};
+
+const mostrarMensajeResultadoPosible = () => {
+  let mensaje: string = `Tu puntuación habría sido ${puntuacion}`;
+
+  if (puntuacion < 7.5) {
+    mostrarMensaje(mensaje);
+  }
+
+  if (puntuacion > 7.5) {
+    mensaje = `${puntuacion} Te habrías pasado 🪦 GAME OVER`;
+    mostrarMensaje(mensaje);
+  }
+  if (puntuacion === 7.5) {
+    mensaje = `${puntuacion} Habrías ganado! 🫠`;
+    mostrarMensaje(mensaje);
+  }
+};
+
+const mostrarQueHabriaPasado = () => {
+  mostrarResultadoPosible();
+  mostrarMensajeResultadoPosible();
+};
+
+const mostrarResultadoPosible = () => {
+  dameCarta();
+};
